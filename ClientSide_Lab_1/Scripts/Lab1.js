118ee35c0ceccff29442ee21457b883e6a85beb6
Scien <timothy.brostrom@gmail.com>
2016-01-03 21:19:37 +0100

    Gjort klart uppgift 2

--- ClientSide_Lab_1/Scripts/Lab1.js
+++ ClientSide_Lab_1/Scripts/Lab1.js
@@ -6,4 +6,11 @@
     document.entire_site.calculatedroot.value = result;
 }
 
-function CalculateFullPay()
+function CalculateFullPay() {
+    var basicpay = 15000;
+    var sales = document.entire_site.salesinput.value;
+
+    var fullpay = 15000 + Math.round(sales * 0.09);
+
+    document.entire_site.calculatedpay.value = fullpay;
+}
