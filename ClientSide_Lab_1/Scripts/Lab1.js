556349237b2bbcb8b10366efb13b2ab531e6469b
Scien <timothy.brostrom@gmail.com>
2016-01-03 22:10:28 +0100

    Testar Unobtrusive Javascript med eventhandlers Uppgift 3

--- ClientSide_Lab_1/Scripts/Lab1.js
+++ ClientSide_Lab_1/Scripts/Lab1.js
@@ -1,4 +1,6 @@
-﻿function CalculateSquareRoot(){
+﻿//Uppgift 1
+
+function CalculateSquareRoot() {
     var numberinput1 = document.entire_site.numberinput.value;
 
     var result = Math.sqrt(numberinput1);
@@ -6,6 +8,7 @@
     document.entire_site.calculatedroot.value = result;
 }
 
+//Uppgift 2
 function CalculateFullPay() {
     var basicpay = 15000;
     var sales = document.entire_site.salesinput.value;
@@ -13,4 +16,24 @@ function CalculateFullPay() {
     var fullpay = 15000 + Math.round(sales * 0.09);
 
     document.entire_site.calculatedpay.value = fullpay;
+}
+
+//Uppgift 3
+var convertedminutes = document.getElementById('convertedminutes');
+minutes.addEventListener('click', ConvertHoursToMinutes, false);
+
+var convertedseconds = document.getElementById('convertedseconds');
+minutes.addEventListener('hover', ConvertHoursToseconds, false);
+
+function ConvertHoursToMinutes() {
+    var hours = document.getElementById('hourinput').value;
+
+    var convertToMinutes = hours * 60;
+
+    document.getElementById('convertedminutes').innerHTML = convertToMinutes;
+
+}
+
+function ConvertHoursToSeconds() {
+
 }
